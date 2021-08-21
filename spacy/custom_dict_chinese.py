ee6ab0be3e0ae15fff3e11dafbb417873e802986
jeffzhengye <3420661@qq.com>
2021-08-21 16:35:39 +0800

    add spacy chinese examples

---
+++ spacy/custom_dict_chinese.py
@@ -0,0 +1,46 @@
+import spacy
+
+"""
+1. 如何加入自定义词典
+2. 如何设置default segmenter
+"""
+
+
+def test_custom_dict():
+    nlp = spacy.load('zh_core_web_sm')
+    proper_nouns = ['给水流量', '蒸汽流量', '过热度', '主蒸汽']
+    nlp.tokenizer.pkuseg_update_user_dict(proper_nouns)
+
+    doc = nlp('调整给水，注意给水流量与蒸汽流量相匹配，注意过热度，保证主蒸汽温度不超限。')
+    print('/'.join([t.text for t in doc]))
+
+
+def test_switch_segmenter():
+    """
+    2. 如何设置default segmenter
+    :return:
+    """
+    from spacy.lang.zh import Chinese
+    s = '调整给水，注意给水流量与蒸汽流量相匹配，注意过热度，保证主蒸汽温度不超限。This is not perfect'
+    # Character segmentation (default)
+    nlp = Chinese()
+    # char
+    cfg = {"segmenter": "char"}
+    nlp = Chinese.from_config({"nlp": {"tokenizer": cfg}})
+    doc = nlp(s)
+    print('/'.join([t.text for t in doc]))
+    # Jieba
+    cfg = {"segmenter": "jieba"}
+    nlp = Chinese.from_config({"nlp": {"tokenizer": cfg}})
+    doc = nlp(s)
+    print('/'.join([t.text for t in doc]))
+
+    # PKUSeg with "mixed" model provided by pkuseg
+    cfg = {"segmenter": "pkuseg"}
+    nlp = Chinese.from_config({"nlp": {"tokenizer": cfg}})
+    nlp.tokenizer.initialize(pkuseg_model="mixed")
+    doc = nlp(s)
+    print('/'.join([t.text for t in doc]))
+
+
+test_switch_segmenter()
