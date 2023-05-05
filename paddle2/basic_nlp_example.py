77935cc0cc5ef094f5020a9501e8ed7293eea2b9
jeffzhengye <jeffzhengye@gmail.com>
2023-05-05 22:38:27 +0800

    update llm

--- paddle2/basic_nlp_example.py
+++ paddle2/basic_nlp_example.py
@@ -1,7 +1,6 @@
 import paddle
 import paddlenlp as ppnlp
 
-
 clip = paddle.nn.ClipGradByValue(max=10.)
 optimizer = paddle.optimizer.AdamW(learning_rate=1e-6, parameters=model.parameters())
 lr_scheduler = ppnlp.transformers.CosineDecayWithWarmup(learning_rate, num_training_steps, warmup_proportion)
